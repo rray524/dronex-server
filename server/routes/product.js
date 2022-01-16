9fc8aaae35e671cfaef823135047537455a69ce7
rray524 <rray524@gmail.com>
2022-01-16 12:23:47 +0600

    single product view & carousel

--- server/routes/product.js
+++ server/routes/product.js
@@ -11,13 +11,22 @@ const {
     remove,
     read,
     update,
+    list,
+    productsCount,
+    productStar,
 } = require("../controllers/product");
 
 // routes
 router.post("/product", authCheck, adminCheck, create);
+router.get("/products/total", productsCount);
+
 router.get("/products/:count", listAll); // products/100
 router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
+
+router.post("/products", list);
+// rating
+router.put("/product/star/:productId", authCheck, productStar);
 
 module.exports = router;
